9d5324056975be1054b332d5f60d3d30073ea88d
cvp <cvphuoc2014@gmail.com>
2023-05-15 22:30:00 +0700

    Components Part 1 second

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer class="footer">
+                <p>copyright @ 2022</p>
+            </footer>
+        )
+    }
+}
