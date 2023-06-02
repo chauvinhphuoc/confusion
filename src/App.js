d7f4c602638e30acd05ddd3311fccf3b042d9c9f
cvp <cvphuoc2014@gmail.com>
2023-06-02 20:58:19 +0700

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Navigation from './components/Navigation';
-import Player from './components/player';
 import Footer from './components/Footer';
+import Main from './components/Main';
+import Navigation from './components/Navigation';
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Navigation/>
-      <Player/>
+      <Main/>
       <Footer/>
     </div>
   );
